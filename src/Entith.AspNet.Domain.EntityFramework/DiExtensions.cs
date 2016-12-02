4acd4cfb6db6d41c24c70a44d70aaa0206ebb7a4
entith <entith@entith.net>
2016-12-02 12:24:04 -0500

    Added extension methods for easier repo DI registration.

---
+++ src/Entith.AspNet.Domain.EntityFramework/DiExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Entith.AspNet.Domain.DependencyInjection;
+using Entith.AspNet.Domain.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entith.AspNet.Domain.DependencyInjection
+{
+    public static class DiExtensions
+    {
+        public static IEntityBuilder<TEntity, TKey> WithDefaultEfRepository<TEntity, TKey>(this IEntityBuilder<TEntity, TKey> builder)
+            where TEntity : class, IEntity<TKey>
+            where TKey : IEquatable<TKey>
+        {
+            return builder.WithRepository<IEfRepository<TEntity, TKey>>();
+        }
+    }
+}
